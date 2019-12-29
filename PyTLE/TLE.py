103c7b24d251c9c0b571e2562ff977775e29f568
Kerry N. Wood <kerry.wood@jhuapl.edu>
2019-12-29 06:44:48 -0700

    SGP4 satrec to TLE

--- PyTLE/TLE.py
+++ PyTLE/TLE.py
@@ -10,6 +10,10 @@ from astropy.time import Time as astro_date
 # and do not include a checksum.  Since we don't check checksum anyways,
 # ignore it.  (Commented out in fields dictionary below).
 
+# for SGP4 satrec
+deg2rad  =   math.pi / 180.0         #    0.0174532925199433
+xpdotp   =  1440.0 / (2.0 *math.pi)  #  229.1831180523293
+
 # expoRE = re.compile("([\+\-\ ]?[0-9]{4,6})([\+\-]?[0-9]{1,2})")
 launch_year_re = re.compile(r'(\d{2})\d{2,3} *')
 launch_number_re = re.compile(r'\d{2}(\d{2,3}) *')
@@ -102,7 +106,7 @@ def get_tle_datatype(VAL, TYPE):
     # try: return launch_piece_re.match( VAL ).groups()[0]
             # except: print "Could not convert", VAL, "to launch_piece"
 
-    return None;
+    return None
 
 ##########################################################################
 # list the fields that will populate the struct here.  This also lists the
@@ -145,9 +149,9 @@ class tle_class:
     """ This currently does NO error checking (even checksum).  Next version"""
 
     def _set_tle_fields(self):
-        map(lambda x: setattr(self, x, None), tle_fields['line1'].keys())
-        map(lambda x: setattr(self, x, None),
-        tle_fields['line2'].keys())
+        for key in tle_fields['line1'] : setattr( self, key, None )
+        for key in tle_fields['line2'] : setattr( self, key, None )
+      
 
     def _clear(self):
         self.error = 0
@@ -181,14 +185,12 @@ class tle_class:
             self._err_msg( 'unexpected error in _derive_dates')
             formatExceptionInfo()
             self.error = 1
+        if self.epoch_year <= 57: self.epoch_full_year = self.epoch_year + 2000
+        else:   self.epoch_full_year = self.epoch_year + 1900
+# try: 
         return
-        if self.epoch_year <= 57:
-            self.epoch_full_year = self.epoch_year + 2000
-        else:
-            self.epoch_full_year = self.epoch_year + 1900
-                        # try: 
-            self._astro_date = astro_date( 
-                tle_epoch.year_day_to_datetime( self.epoch_year, self.epoch_day) )
+        self._astro_date = astro_date(tle_epoch.year_day_to_datetime( self.epoch_year, self.epoch_day) )
+           
 
     def printFields( self ):
         A = str()
@@ -235,21 +237,18 @@ class tle_class:
         while len(self.line1) < 69 : self.line1+='0'
         while len(self.line2) < 69 : self.line2+='0'
 
-    def __init__(self, line1, line2, catalogName='N/A'):
+    def __init__(self, line1=None, line2=None, catalogName='N/A'):
         self._clear()
+        if line1 == None or line2 == None: return 
         self.catalog_file = catalogName
-        self._parse_line( 'line1', line1 )
-        self._parse_line( 'line2', line2 )
-        if self.error == 1: return
-        self._derive_dates()
-        self._check_lines()
-        self._calculate_apogee_perigee()
+        self.from_lines( line1, line2 )
 
     def _generate_new_tle( self ):
         ''' generate_new_tle(): we can modify the TLE values stored in this class, and generate a representative TLE.
                     Note, that the original TLE lines are still stored in line1, line2
                     --> NOTE: we are currently kluging checksums by just appending a zero'''
-        return self._generate_new_line1() + '\n' + self._generate_new_line2()
+        try: return self._generate_new_line1() + '\n' + self._generate_new_line2()
+        except: return None
 
     def _generate_new_line1( self ):
         rV = '1 {:05d}U {:02d}{:03d}{:3s} {:02d}{:012.8f} '.format(self.sat_no,self.launch_year,self.launch_number,self.launch_piece,self.epoch_year,self.epoch_day)
@@ -283,6 +282,66 @@ class tle_class:
         self.perigee = ( semi_major * (1 - self.eccentricity) ) - earth_rad
         self.apogee =  ( semi_major * (1 + self.eccentricity) ) - earth_rad
 
+    def from_lines( self, line1, line2 ):
+        self._parse_line( 'line1', line1 )
+        self._parse_line( 'line2', line2 )
+        self._derive_dates()
+        self._check_lines()
+        self._calculate_apogee_perigee()
+
+    def from_sgp4_satrec( self, satrec ):
+        ''' 
+        Brandon Rhode's SGP4 Python code is a direct port from Vallado's code, and outputs a 'satrec' structure after
+        parsing a TLE.  Let's see if we can use that to serialize as well
+
+        Note that some fields are converted immediately by SGP4 on ingest, so we'll convert them back (this serializer
+        does *not* keep units in the code, just raw fields
+
+        #  ---- find no, ndot, nddot ----
+        satrec.no_kozai = satrec.no_kozai / xpdotp; #   rad/min
+        satrec.nddot= satrec.nddot * pow(10.0, nexp);
+        satrec.bstar= satrec.bstar * pow(10.0, ibexp);
+
+        #  ---- convert to sgp4 units ----
+        satrec.ndot = satrec.ndot  / (xpdotp*1440.0);  #   ? * minperday
+        satrec.nddot= satrec.nddot / (xpdotp*1440.0*1440);
+
+        #  ---- find standard orbital elements ----
+        satrec.inclo = satrec.inclo  * deg2rad;
+        satrec.nodeo = satrec.nodeo  * deg2rad;
+        satrec.argpo = satrec.argpo  * deg2rad;
+        satrec.mo    = satrec.mo     * deg2rad;
+        '''
+        self.sat_no = satrec.satnum
+        try: self.classificaiton = satrec.classification
+        except: self.classification = 'U'
+        try: self.launch_year = launch_year( satrec.intldesg) 
+        except: self.launch_year = 99
+        try: self.launch_number = launch_number( satrec.intldesg )
+        except: self.launch_number = 1
+        try: self.launch_piece = launch_piece( satrec.intldesg )
+        except: self.launch_piece = 'A'
+        self.epoch_full_year =  satrec.epochyr
+        if self.epoch_full_year >= 2000: self.epoch_year = self.epoch_full_year - 2000
+        if self.epoch_full_year < 2000: self.epoch_year = self.epoch_full_year - 1900
+        self.epoch_day = satrec.epochdays
+        self.mean_motion_1 = satrec.ndot * (xpdotp*1440.0)          # <--- converted
+        self.mean_motion_2 = satrec.nddot *(xpdotp*1440.*1440.)     # <--- converted
+        self.bstar = satrec.bstar
+        try: self.ephem_type = satrec.ephtype
+        except: self.ephem_type = 0
+        try: self.element_number = satrec.elnum
+        except: self.element_number = 1
+        self.inclination = math.degrees( satrec.inclo )
+        self.ra = math.degrees( satrec.nodeo )
+        self.eccentricity = satrec.ecco
+        self.argument = math.degrees( satrec.argpo )
+        self.mean_anomaly = math.degrees( satrec.mo )
+        self.mean_motion = satrec.mdot * xpdotp                 # <--- converted
+        try: self.rev_number = satrec.revnum 
+        except: self.rev_number = 0
+        return self
+
 
 if __name__=="__main__":
     L1="1 37257U 10068B   10353.59408145  .00648834 -26028-4  40304-3 0    55"
@@ -294,13 +353,21 @@ if __name__=="__main__":
     L1 = "1 27969U 88085P   03315.93426859 +.00400218 +00000-0 +53521-0 0 0120"
     L2 = "2 27969 065.3095 204.8471 5716983 077.9662 346.7271 04.3220364100781"
 
-    print('Original:\n\t{}\n\t{}'.format(L1,L2))
     A = tle_class( L1, L2, catalogName='blah')
-    print('New:\n\t{}\n\t{}'.format(L1,L2))
-    print(A)
-    A.inclination += 0.5
-    print(A)
+    print("--------------->", A.mean_motion )
+    print("--------------->", A.mean_motion / xpdotp )
+    print(L1)
+    print(L2)
 
+    from sgp4.io import twoline2rv 
+    import sgp4.earth_gravity as EG
+
+    satrec = twoline2rv( L1, L2, EG.wgs72 )
+    B = tle_class()
+    B.from_sgp4_satrec( satrec )
+    print(B)
+
+    sys.exit()
     L1 = "1 00043U 60006  A 74037.77776050  .00988543 +09018-2 +01753-4 0 09299"
     L2 = "2 00043 033.0454 016.0357 0002584 233.4948 126.5498 16.39800644771946"
     L1 = '1 00820U 64030  B 64219.95713468  .05697386 +00000-0 +00000-0 0 00083'
